956f7ce059348e2ef1df03a88de13a1d9c9f65a1
Adran-Nailo <onfrobotics@gmail.com>
2024-06-21 11:34:08 -0500

    Add files via upload

--- personal/color_testing.py
+++ personal/color_testing.py
@@ -1,11 +1,13 @@
-class color:
-    default = "\033[0m"
-    green = "\033[32m"
-    purple = "\033[95m"
-    blue = '\033[94m'
-    cyan = '\033[96m'
-    yellow = '\033[93m'
-    red = '\033[91m'
-    clear = '\033[0m'
-    bold = '\033[1m'
-    underrline = '\033[4m'
+class Color:
+
+    def __init__(self):
+        self.default = "\033[0m"
+        self.green = "\033[32m"
+        self.purple = "\033[95m"
+        self.blue = '\033[94m'
+        self.cyan = '\033[96m'
+        self.yellow = '\033[93m'
+        self.red = '\033[91m'
+        self.clear = '\033[0m'
+        self.bold = '\033[1m'
+        self.underline = '\033[4m'
