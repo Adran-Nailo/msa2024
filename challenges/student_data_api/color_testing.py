574e62d4aa0287836cd95fd0a3b0fbc20d9e28a5
Adran-Nailo <onfrobotics@gmail.com>
2024-06-25 11:20:11 -0500

    Add files via upload

---
+++ challenges/student_data_api/color_testing.py
@@ -0,0 +1,14 @@
+class Color:
+
+    def __init__(self):
+        self.default = "\033[0m"
+        self.green = "\033[32m"
+        self.purple = "\033[95m"
+        self.blue = '\033[94m'
+        self.cyan = '\033[96m'
+        self.yellow = '\033[93m'
+        self.red = '\033[91m'
+        self.clear = '\033[0m'
+        self.bold = '\033[1m'
+        self.underline = '\033[4m'
+        self.error = "\033[91;1;4m"
---
+++ challenges/student_data_api/web_api.py
@@ -0,0 +1,29 @@
+import flask
+from flask import request, jsonify
+import student_generator_v2 as sg
+
+#create a flask app
+app = flask.Flask(__name__)
+
+#use student generator as a base
+#get student dictionaries
+#create 2 routes
+#return all students
+#return students by major
+
+#Auto reload server when changes are made
+app.config["DEBUG"] = True
+
+#get the student dictionaries
+student_dictionaries = sg.get_student_dictionaries()
+
+#create a route for the server homepage
+@app.route("/", methods = ["GET"])
+def index():
+    return "<h1>Hello World, my name is Oliver Frazier</h1"
+#create a route to return all student's data in JSON format
+@app.route("/api/students/all", methods = ["GET"])
+def api_all():
+    return jsonify(student_dictionaries)
+
+app.run(port=5010)
