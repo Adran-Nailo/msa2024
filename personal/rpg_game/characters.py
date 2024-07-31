4a5213deb17bffcd88774079319b31976cf5d597
Adran-Nailo <onfrobotics@gmail.com>
2024-07-30 23:53:34 -0500

    Add files via upload

--- personal/rpg_game/characters.py
+++ personal/rpg_game/characters.py
@@ -1,9 +1,14 @@
 class Characters():
-    def __init__(self,position_1,position_2,score,icon):
+    def __init__(self,position_1,position_2,score,icon,current_map):
         self.position_1 = position_1
         self.position_2 = position_2
         self.coordinates_x = position_1
         self.coordinates_y = position_2
         self.score = score
         self.icon = icon
-        self.position_list = [position_1,position_2]
+        self.position_list = [self.coordinates_y,self.coordinates_x]
+        self.current_map = current_map
+        self.sword_count = 0
+        self.potion_count = 0
+        self.wall_count = 0
+        self.render_distance = 3
